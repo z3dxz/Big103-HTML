f019c7f5ada3eee7958ddfaa144e7dc70a325ce4
z3dxz <3dxz2x@gmail.com>
2025-07-01 06:12:36 -0400

    title,artist

--- src/starter.cpp
+++ src/starter.cpp
@@ -11,15 +11,25 @@ int main(int argc, char *argv[]){
 
     bool ui = true;
     bool get = false;
+    bool gett = false;
+    bool geta = false;
 
     int i = 0;
     while (i < argc) {
         if(i == 1) {
-            std::string getstr = "get";
-            if(std::string(argv[i]) == getstr) {
+            if(std::string(argv[i]) == "get") {
                 ui = false;
                 get = true;
             }
+            if(std::string(argv[i]) == "title") {
+                ui = false;
+                gett = true;
+            }
+            if(std::string(argv[i]) == "artist") {
+                ui = false;
+                geta = true;
+            }
+
         }
 
         i++;
@@ -29,6 +39,18 @@ int main(int argc, char *argv[]){
         std::string at, aa;
         std::string r = request(&at, &aa);
         std::cout << at << " - " << aa;
+    }
+
+    if(gett) {
+        std::string at, aa;
+        std::string r = request(&at, &aa);
+        std::cout << at;
+    }
+
+    if(geta) {
+        std::string at, aa;
+        std::string r = request(&at, &aa);
+        std::cout << aa;
     }
 
     if(ui) {
